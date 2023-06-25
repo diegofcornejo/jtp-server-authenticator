5f59595ef81b40339f74eaedfb61e2271d80169b
Diego Cornejo <diegof.cornejo@gmail.com>
2023-06-24 23:33:28 -0600

    Update welcome message and token preview style

--- pages/login.js
+++ pages/login.js
@@ -143,8 +143,8 @@ const Login = () => {
 						)}
 						{token && (
 							<>
-								<p className="text-sm text-green-500">Welcome {nick}!</p>
-								<p className="text-lg break-all">{token}</p>
+								<p className="text-lg text-green-500">Welcome {nick}!</p>
+								<p className="text-sm break-all">{token}</p>
                                 <button
                                     className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-4 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                                     onClick={handleCopyToken}
